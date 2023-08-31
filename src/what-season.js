1898c8549bda61f4d1af695458f1850e5f32568b
Nikita <nikita119@protonmail.com>
2023-08-31 16:27:53 +0300

    3 task completed

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-    //throw new CustomError('Not implemented');
-    // return throw new CustomError('Not implemented');
+    if (!date) {
+        return 'Unable to determine the time of year!'
+    }
+    const month = date.getMonth() + 1;
+    if (month > 12 && month < 1 || typeof month !== 'number') {
+         throw new Error('Error');
+    }
+    try {
+        date.getTime();
+    } catch(err) {
+        throw new Error('ERROR');
+    }
+    switch(month) {
+        case 12:
+        case 1:
+        case 2:
+            return 'winter';
+        case 3:
+        case 4:
+        case 5:
+            return 'spring';
+        case 6:
+        case 7:
+        case 8:
+            return 'summer';
+        case 9:
+        case 10:
+        case 11:
+           return 'fall';
+    }
 };
